5709dda475a406175a0914d879b1f84b09c3a0e2
Dörthe Arndt <dorthe.arndt@ugent.be>
2020-03-18 18:42:02 +0100

    changed prefixes and gave meaningful names

---
+++ demo/run.sh
@@ -0,0 +1,137 @@
+#!/bin/sh
+
+#first we produce the steps on each level based on the OSLO steps descriptions
+eye oslo-descriptions/change-address-steps.ttl oslo-descriptions/change-address-states.ttl oslo-descriptions/change-address-shapes.ttl translation/step-reasoning.n3 translation/aux.n3 translation/createPattern.n3 --query translation/mappingRuleCreationJourney.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ >interim/journey-level-steps.n3
+
+
+eye oslo-descriptions/change-address-steps.ttl oslo-descriptions/change-address-states.ttl oslo-descriptions/change-address-shapes.ttl translation/step-reasoning.n3 translation/aux.n3 translation/createPattern.n3 --query translation/mappingRuleCreationContainer.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ >interim/container-level-steps.n3
+
+eye oslo-descriptions/change-address-steps.ttl oslo-descriptions/change-address-states.ttl oslo-descriptions/change-address-shapes.ttl translation/step-reasoning.n3 translation/aux.n3 translation/createPattern.n3 --query translation/mappingRuleCreationComponent.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ >interim/component-level-steps.n3
+
+
+#################################################
+# Journey Level
+#################################################
+
+# Preselection step based on the goal
+eye  interim/journey-level-steps.n3 profile/knowledge.n3 --query preselection/pregeneration.n3 --nope >interim/shortJourneyDescriptions.n3
+
+
+eye interim/journey-level-steps.n3 interim/shortJourneyDescriptions.n3 journey/journeyGoal.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_Journey.n3
+
+#To compute the highest level workflow use:
+eye workflow-composer/gps-plugin_modified.n3 profile/knowledge.n3 interim/selectedSteps_Journey.n3 oslo-descriptions/change-address-steps.ttl --query journey/journeyGoal.n3 --nope > interim/all_journey-paths.n3
+
+#To compute the highest level workflow without permutations use 
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/selectedSteps_Journey.n3 oslo-descriptions/change-address-steps.ttl --query journey/journeyGoal.n3 --nope > interim/journey-paths_noPermutations.n3
+
+#################################################
+# Container Level
+###################################################
+
+#for our preselection steps we need short descriptions
+eye  interim/container-level-steps.n3 profile/knowledge.n3 --query preselection/pregeneration.n3 --nope > interim/shortContainerDescriptions.n3
+
+
+#
+# Goal1: address change
+#
+#
+
+# we use backwards reasoning to see which steps can lead to the goal (this step does not need to be repeated whenever we use the goal, only if either the steps or the goal changes).
+#eye  container-level-steps.n3 knowledge.n3 --query Preselection/pregeneration.n3 --nope >shortContainerDescriptions.n3
+#eye container-level-steps.n3 shortContainerDescriptions.n3 containerGoal_addressChange.n3 Preselection/preselection.n3 knowledge.n3 --nope --query Preselection/prequery.n3 >AddressChangeSelected.n3
+
+
+#eye gps-plugin_modified_noPermutations.n3 knowledge.n3 AddressChangeSelected.n3 change-address-steps.ttl --query containerGoal_addressChange.n3 --nope > container-address-paths.n3
+
+
+#
+# Goal Waste collection
+#
+
+
+
+
+# create goal based on step name
+eye choices/selected_Waste.n3 interim/journey-level-steps.n3 --query subgoals/subgoalCreation.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ > interim/createdGoal_Waste.n3
+
+#create a dummy step which creates the input data of the step (this is needed if we want to create sub-workflows for steps which rely on others).
+eye choices/selected_Waste.n3 interim/journey-level-steps.n3 --query subgoals/creationOfRuleForMissingData.n3 --nope  > interim/extraRule_waste.n3
+#todo: if this rule adds data, it should be visible in the output as one step
+
+
+#preselect
+eye interim/container-level-steps.n3 interim/shortContainerDescriptions.n3 interim/createdGoal_Waste.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_waste.n3
+
+#create path
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/extraRule_waste.n3 subgoals/aux2.n3 interim/selectedSteps_waste.n3 oslo-descriptions/change-address-steps.ttl --query interim/createdGoal_Waste.n3 --nope > interim/WasteContainer_noPermutation.n3
+
+
+
+#
+# Goal Request address change
+#
+
+
+# create goal based on step name
+eye choices/selected_AddressSubmission.n3 interim/journey-level-steps.n3 --query subgoals/subgoalCreation.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ > interim/createdGoal_address.n3
+
+#create rule to add input info for the step
+eye choices/selected_AddressSubmission.n3 interim/journey-level-steps.n3 --query subgoals/creationOfRuleForMissingData.n3 --nope >interim/extraRule_addresschange.n3
+
+#preselect
+eye interim/container-level-steps.n3 interim/shortContainerDescriptions.n3 interim/createdGoal_address.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_addresschange.n3
+
+#createPath
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/extraRule_addresschange.n3 subgoals/aux2.n3 interim/selectedSteps_addresschange.n3 oslo-descriptions/change-address-steps.ttl --query interim/createdGoal_address.n3 --nope > interim/ChangeAddress_noPermutation.n3
+
+
+#####################################################
+# Component level
+##################################################
+eye  interim/component-level-steps.n3 profile/knowledge.n3 --query preselection/pregeneration.n3 --nope >interim/shortComponentDescriptions.n3
+
+#
+# Policevisit
+#
+
+# create goal based on step name
+eye choices/selected_Policevisit.n3 interim/container-level-steps.n3 --query subgoals/subgoalCreation.n3 --nope --no-skolem http://josd.github.io/.well-known/genid/ > interim/createdGoal_police.n3
+
+# create rule to add input info for the step
+eye choices/selected_Policevisit.n3 interim/container-level-steps.n3 --query subgoals/creationOfRuleForMissingData.n3 --nope >interim/extraRule_police.n3
+
+#preselect
+eye interim/component-level-steps.n3 interim/shortComponentDescriptions.n3 interim/createdGoal_police.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_policevisit.n3
+
+#createPath
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/extraRule_police.n3 subgoals/aux2.n3 interim/selectedSteps_policevisit.n3 oslo-descriptions/change-address-steps.ttl --query interim/createdGoal_police.n3 --nope > interim/Policevisit_noPermutation.n3
+
+
+#
+# office visit to change id
+#
+#TODO
+
+#################################3
+#to be adjusted, here a container level is still missing (is that necessary?)
+
+eye interim/component-level-steps.n3 interim/shortComponentDescriptions.n3 interim/goal_personalInformation.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_personalInfo.n3
+
+
+#here, we get the output with all permutations, that is not really what we want, therefore that is commented out
+#eye gps-plugin_modified.n3 knowledge.n3 PersonalInfoSelected.n3 NewSteps.n3 --query Goal_personalInformation.n3 --nope > personalInfo-paths.n3
+
+
+# personalInfo
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/SelectedSteps_personalInfo.n3 oslo-descriptions/change-address-steps.ttl --query interim/goal_personalInformation.n3 --nope > interim/personalInfo_noPermutation.n3
+
+#moving data
+eye interim/component-level-steps.n3 interim/shortComponentDescriptions.n3 interim/Goal_provideMovingData.n3 preselection/preselection.n3 profile/knowledge.n3 --nope --query preselection/prequery.n3 >interim/selectedSteps_movingData.n3
+
+
+eye workflow-composer/gps-plugin_modified_noPermutations.n3 profile/knowledge.n3 interim/selectedSteps_movingData.n3 oslo-descriptions/change-address-steps.ttl --query interim/Goal_provideMovingData.n3 --nope > interim/movingData_noPermutation.n3
+
+
+
